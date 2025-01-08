d5ddfffd6f2b4e7f2d034c404d01a21ff8676e06
tlvince-sandbox <34712945+tlvince-sandbox@users.noreply.github.com>
2025-01-08 09:45:54 +0000

    sha

--- cm3588.nix
+++ cm3588.nix
@@ -16,7 +16,7 @@
         name = "crypto: rockchip: add support for rk3588/rk3568";
         patch = pkgs.fetchpatch {
           url = "file://${./patches/rk35xx-montjoie-crypto-v2-rk35xx.patch}";
-          sha256 = lib.fakeSha256;
+          sha256 = "sha256-ANLKsu5IPEFZLbAnEylkRz/1XtKih0VbB5cO41+6QdM=";
         };
       }
       {
